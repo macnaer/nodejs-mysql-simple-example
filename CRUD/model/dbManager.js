348fda926dd99009ba5613c5d3300b9c86504641
Trofimchuk <k2@mail.local>
2019-11-25 00:06:23 +0200

    Refactor Chackuser functions

--- CRUD/model/dbManager.js
+++ CRUD/model/dbManager.js
@@ -2,54 +2,34 @@ const mysql = require("mysql");
 
 // MYSQL Server
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "company"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "company"
 });
 
 db.connect(err => {
-    if (err) {
-        throw err;
-        console.log("Connection error => ", err);
-    }
-    console.log("Mysql server connected...");
+  if (err) {
+    throw err;
+    console.log("Connection error => ", err);
+  }
+  console.log("Mysql server connected...");
 });
 
+CheckUser = (username, callback) => {
+  let sql = "SELECT login FROM users WHERE login LIKE '" + username + "'";
+  db.query(sql, (err, result) => {
+    if (err) {
+    } else if (result.length != 0) {
+      console.log("true");
+      return callback("true");
+    } else {
+      console.log("false");
+      return callback("false");
+    }
+  });
+};
 
-exports.CheckUser =  (formData) => {
-    let msg = "DBManager!";
-    let sql = "SELECT login FROM users WHERE login LIKE \'" + formData.username + "\'";
-    db.query(sql, (err, result) => {
-        if (err) {
-            console.log("First place!");
-            msg = err;
-            console.log(msg);
-        }
-        else {
-            if (result.length == 0) {
-                const sql = "INSERT INTO `users` SET ? ";
-                const data = {
-                    login: formData.username,
-                    email: formData.email,
-                    password: formData.password
-                }
-                db.query(sql, data, (err, result) => {
-                    if (err) {
-                        msg = arr;
-                        console.log(msg);
-                    }
-                    else {
-                        msg = "<h2>User created!</h2>";
-                        console.log(msg);
-                    }
-                })
-            }
-            else {
-                msg = "<h2>User exist.</h2>"
-                console.log("Inside", msg);
-            }
-        }
-    });
-    return msg;
-}
+module.exports = {
+  CheckUser
+};
--- CRUD/routes/register.js
+++ CRUD/routes/register.js
@@ -2,31 +2,29 @@ const express = require("express");
 const path = require("path");
 const { CheckUser } = require("../model/dbManager");
 
-
 const router = express.Router();
 
 router.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "../" ,"views", "register.html"));
+  res.sendFile(path.join(__dirname, "../", "views", "register.html"));
 });
 
-router.post("/register", (req,res) => {
-    const formData = {
-        username: req.body.login,
-        email: req.body.email,
-        password: req.body.password,
-        repassword: req.body.repassword
-    }
+router.post("/register", (req, res) => {
+  const formData = {
+    username: req.body.login,
+    email: req.body.email,
+    password: req.body.password,
+    repassword: req.body.repassword
+  };
 
-    if (formData.password != formData.repassword){
-        res.send("Password not match!");
-    }
-    else{
-        let checkUserResult = CheckUser(formData);
-        // console.log("Outside", checkUserResult);
-        res.send(checkUserResult);
-
-    }
-  
+  if (formData.password != formData.repassword) {
+    res.send("Password not match!");
+  } else {
+    CheckUser(formData.username, function(result) {
+      let checkUserResult = result;
+      console.log("Outside", checkUserResult);
+      res.send(`<h1>${checkUserResult}</h1>`);
+    });
+  }
 });
 
 module.exports = router;
